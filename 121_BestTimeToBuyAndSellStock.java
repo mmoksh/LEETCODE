e70732615eed387405a86888f63054c1f6747f28
Mohammed Alokshiya <mm-oksh@hotmail.com>
2017-10-26 23:27:16 -0500

    Create 121_BestTimeToBuyAndSellStock.java

---
+++ 121_BestTimeToBuyAndSellStock.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        if (prices.length == 0) {
+            return 0;
+        }
+        int min = prices[0];
+        int max_rev = 0;
+        for (int i = 0; i < prices.length; i++) {
+            max_rev = Math.max(max_rev, prices[i] - min);
+            min = Math.min(min, prices[i]);
+        }
+        return max_rev;
+    }
+}
