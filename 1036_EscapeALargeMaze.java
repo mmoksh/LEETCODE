4821aa0f1aad1e7e47a0cc76a0dc2b143ec9cb2a
Mohammed Alokshiya <mm-oksh@hotmail.com>
2019-05-04 01:57:19 +0300

    Update 1036_EscapeALargeMaze.java

--- 1036_EscapeALargeMaze.java
+++ 1036_EscapeALargeMaze.java
@@ -1,42 +1,31 @@
 class Solution {
-    public boolean isEscapePossible(int[][] blocked, int[] source, int[] target) {
-        return isEscapePossible2(blocked, source, target) && isEscapePossible2(blocked, target, source);
-    }
-    
-    public boolean isEscapePossible2(int[][] blocked, int[] source, int[] target) {
+    public static boolean isEscapePossible(int[][] blocked, int[] source, int[] target) {
         HashSet<Long> visited = new HashSet<>();
         for (int[] is : blocked) {
             visited.add(is[0] * 1000000L + is[1]);
         }
-        Queue<Integer> q = new LinkedList<>();
-        q.add(source[0]);
-        q.add(source[1]);
-        visited.add(source[0] * 1000000L + source[1]);
-        int steps = 0;
-        int x, y, a, b;
-        long c;
-        int [] d = {1, 0, -1, 0, 1};
-        int size;
-        while (!q.isEmpty()) {
-            size = q.size() / 2;
-            steps++;
-            if (steps > 200) return true;
-            while (size > 0) {
-                size -= 2;
-                x = q.poll();
-                y = q.poll();
-                for (int i = 0; i < 4; i++) {
-                    a = x + d[i];
-                    b = y + d[i + 1];
-                    if (a == target[0] && b == target[1]) return true;
-                    if (a < 0 || b < 0 || a >= 1000000 || b >= 1000000) continue;
-                    c = a * 1000000L + b;
-                    if (!visited.contains(c)) {
-                        visited.add(c);
-                        q.add(a);
-                        q.add(b);
-                    }
-                }
+        HashSet<Long> visited2 = (HashSet<Long>) visited.clone();
+        return dfs(visited, source[0], source[1], source, target)
+                && dfs(visited2, target[0], target[1], target, source);
+    }
+
+    private final static int[] MOVES = {0, 1, 0, -1, 0};
+
+    public static boolean dfs(HashSet<Long> visited, int x, int y, int[] source, int[] target) {
+        long c = x * 1000000L + y;
+        if (x < 0 || y < 0 || x == 1000000 || y == 1000000 || visited.contains(c)) {
+            return false;
+        }
+        visited.add(c);
+        if (x == target[0] && y == target[1]) {
+            return true;
+        }
+        if (Math.abs(x - source[0]) + Math.abs(y - source[1]) >= 200) {
+            return true;
+        }
+        for (int i = 0; i < 4; i++) {
+            if (dfs(visited, x + MOVES[i], y + MOVES[i + 1], source, target)) {
+                return true;
             }
         }
         return false;
