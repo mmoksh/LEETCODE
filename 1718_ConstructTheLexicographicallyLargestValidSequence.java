2ac83ade71ceda6e410dc20d170322bdb1943817
Mohammed Alokshiya <mm-oksh@hotmail.com>
2021-01-10 22:09:47 +0100

    Create 1718_ConstructTheLexicographicallyLargestValidSequence.java

---
+++ 1718_ConstructTheLexicographicallyLargestValidSequence.java
@@ -0,0 +1,34 @@
+class Solution {
+    
+    public static int[] constructDistancedSequence(int n) {
+        int[] output = new int[n * 2 - 1];
+        dfs(0, output, new boolean[n + 1]);
+        return output;
+    }
+    
+    public static boolean dfs(int index, int [] output, boolean[] visited) {
+        if (index == output.length) return true;
+        if (output[index] != 0) return dfs(index + 1, output, visited);
+        for (int i = visited.length - 1; i > 0; i--) {
+            if (visited[i]) continue;
+            if (i == 1) {
+                visited[1] = true;
+                output[index] = 1;
+                if (dfs(index + 1, output, visited)) return true;
+                visited[1] = false;
+                output[index] = 0;
+            } else if (index + i < output.length && output[index] == 0 && output[index + i] == 0) {
+                visited[i] = true;
+                output[index] = i;
+                output[index + i] = i;
+                boolean done = dfs(index + 1, output, visited);
+                if (done) return true;
+                visited[i] = false;
+                output[index] = 0;
+                output[index + i] = 0;
+            }
+        }
+        return false;
+    }
+
+}
